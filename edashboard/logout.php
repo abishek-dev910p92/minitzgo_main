a452301a626156e0986f17ffc19360568482e1d4
hemang500 <heykrishna1999@gmail.com>
2025-03-20 02:50:31 +0530

    clear c-v1

--- edashboard/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // Start the session
-session_destroy(); // Destroy all session data
-header("Location: login"); // Redirect to login.php
-exit(); // Stop script execution
-?>
